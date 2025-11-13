fd02bd8d1880c7a9842f7c74a88448e16c433ea2
AmtazAhmedGazi <amtazahmedgazi@gmail.com>
2025-11-14 05:14:38 +0600

    visited country function added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Country.css";
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries }) => {
   const currencies = Object.keys(country.currencies.currencies);
+
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    // if (visited) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+    // setVisited(visited ? false : true);
+    setVisited(!visited);
+    handleVisitedCountries(country);
+  };
+
   return (
-    <div>
+    <div className={`country ${visited && "country-visited"} `}>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h3>Name: {country.name.common}</h3>
       <p>Currencies: {currencies} </p>
       <p>Capital: {country.capital.capital}</p>
       <p>Population: {country.population.population}</p>
-      <br />
+      <p>
+        Area: {country.area.area}{" "}
+        {country.area.area > 300000 ? "Big Country" : "Small Country"}
+      </p>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
